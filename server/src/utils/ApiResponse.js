33bfd26472e73e777b3e9afc714019b2619f4a9b
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-27 10:02:49 +0530

    fixed minor bugs

--- server/src/utils/ApiResponse.js
+++ server/src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 export default class ApiResponse {
-    constructor(data, message="Success"){
+    constructor(statusCode, data, message="Success"){
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
