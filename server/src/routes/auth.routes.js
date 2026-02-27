294ca8bd0c1f5f3cf1be9f832e5e6072ff0491f2
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-27 08:05:13 +0530

    configured routes to support registerPartner

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/auth.controller.js';
+import { registerUser, registerPartner, loginUser, logoutUser } from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.post("/register", registerUser, registerPartner);
+router.post("/register-partner", registerPartner)
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
