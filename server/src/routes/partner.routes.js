eaa6c6a65a85c7dd6d6064f126d8b3530a7bcb6a
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-02-28 12:31:06 +0530

    created public temp directory

--- server/src/routes/partner.routes.js
+++ server/src/routes/partner.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { addFoodItem } from '../controllers/partner.controller.js';
+import { addFoodItem, addFoodReel } from '../controllers/partner.controller.js';
 import authenticateToken from '../middleware/auth.middleware.js';
 import upload from '../middleware/multer.middeware.js';
 
 const router = Router();
 
 router.post("/addItem", authenticateToken, upload.single("foodimg"), addFoodItem);
+router.post("/addReel", authenticateToken, upload.single("reel"), addFoodReel);
 
 export default router;
