0af664c4e0dbb2f92f8774aba2a5d5a67a7a47c2
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-02 18:02:10 +0530

    created index file to export all pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home/Home';
+import Signin from './Auth/Signin';
+import Signup from './Auth/Signup';
+import About from './About/About';
+import Profile from './Profile/Profile';
+
+export {Home, Signin, Signup, About, Profile};
