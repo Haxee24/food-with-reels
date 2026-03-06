e7e75a4bde5464f074e0e72f5dfe63f0a08612ba
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-06 22:38:01 +0530

    implemented bottom navbar through outlet and nested routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { Signin, Signup, Home } from "../pages";
+import { Layout } from "lucide-react";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        {/* auth routes */}
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup/customer" element={<Signup partner={false} />} />
-        <Route path="/signup/food-partner" element={<Signup partner={true} />} />
+        <Route path="/" element={<Layout/>}>
+          {/* auth routes */}
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup/customer" element={<Signup partner={false} />} />
+          <Route path="/signup/food-partner" element={<Signup partner={true} />} />
 
-        {/* general routes */}
-        <Route index element={<Home/>} />        
+          {/* general routes */}
+          <Route index element={<Home/>} />         
+        </Route> 
       </Routes>
     </Router>
   )
