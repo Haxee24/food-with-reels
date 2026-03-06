e3ee29263632ac44dda05ace8728f47dbedcbfc0
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-06 22:46:01 +0530

    removed navbar from login and register pages

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,11 +6,11 @@ function AppRoutes() {
   return (
     <Router>
       <Routes>
+        {/* auth routes */}
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup/customer" element={<Signup partner={false} />} />
+        <Route path="/signup/food-partner" element={<Signup partner={true} />} />
         <Route path="/" element={<Layout/>}>
-          {/* auth routes */}
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup/customer" element={<Signup partner={false} />} />
-          <Route path="/signup/food-partner" element={<Signup partner={true} />} />
 
           {/* general routes */}
           <Route index element={<Home/>} />         
