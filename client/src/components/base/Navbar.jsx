7690519d80fdd5ce653a31f50f6bc7c25efab063
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-06 22:34:58 +0530

    styled and completed navbar component

--- client/src/components/base/Navbar.jsx
+++ client/src/components/base/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import {NavLink} from "react-router";
+import {Search, PlaySquare, Utensils, User} from 'lucide-react';
 
-function Footer() {
+export default function Navbar(){
+  const LinkStyle =  "flex flex-col items-center justify-center text-xs text-gray-600 dark:text-neutral-400";
+  const activeStyle = "text-orange-500";
+
   return (
-    <div>Footer</div>
+    <div className="fixed bottom-0 left-0 w-full bg-white dark:bg-neutral-900 border-t border-gray-200 dark:border-neutral-800 h-16 flex justify-around items-center md:hidden">
+      <NavLink
+        to="/search"
+        className={({ isActive }) =>
+          `${linkStyle} ${isActive ? activeStyle : ""}`
+        }
+      >
+        <Search size={22} />
+        <span>Search</span>
+      </NavLink>
+
+      <NavLink
+        to="/reels"
+        className={({ isActive }) =>
+          `${linkStyle} ${isActive ? activeStyle : ""}`
+        }
+      >
+        <PlaySquare size={22} />
+        <span>Reels</span>
+      </NavLink>
+
+      <NavLink
+        to="/restaurants"
+        className={({ isActive }) =>
+          `${linkStyle} ${isActive ? activeStyle : ""}`
+        }
+      >
+        <Utensils size={22} />
+        <span>Food</span>
+      </NavLink>
+
+      <NavLink
+        to="/profile"
+        className={({ isActive }) =>
+          `${linkStyle} ${isActive ? activeStyle : ""}`
+        }
+      >
+        <User size={22} />
+        <span>Profile</span>
+      </NavLink>
+
+    </div>
   )
 }
-
-export default Footer
