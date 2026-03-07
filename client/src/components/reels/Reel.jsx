63ed73e14aac8971670e7b5fe4c650f7d3034f6d
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-07 15:04:25 +0530

    added pause play logic on reel on tap

--- client/src/components/reels/Reel.jsx
+++ client/src/components/reels/Reel.jsx
@@ -1,15 +1,28 @@
+import {useRef} from 'react';
+
 export default function Reel({ reel }) {
+  const videoRef = useRef(null);
+  const handleToggle = () => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
   return (
-    <div className="h-screen w-full snap-start bg-black flex items-center justify-center">
+    <div onClick={handleToggle} className="h-screen w-full snap-start bg-black flex items-center justify-center">
       
       <video
+        ref={videoRef}
         src={reel}
         className="h-full w-full object-cover"
         autoPlay
         loop
         muted
         playsInline
-        controls
       />
 
     </div>
