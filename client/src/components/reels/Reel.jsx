8ed01d8648d4b7571b58139d71730c7e70f4a59b
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-11 12:40:09 +0530

    added volume buttons and functionality with additional styling

--- client/src/components/reels/Reel.jsx
+++ client/src/components/reels/Reel.jsx
@@ -1,10 +1,15 @@
 import {useRef, useState} from 'react';
-import {Play, Pause, Volume2} from 'lucide-react';
+import {Play, Pause, Volume2, VolumeX} from 'lucide-react';
 
 export default function Reel({ reel }) {
   const videoRef = useRef(null);
   const [paused, setPaused] = useState(false);
   const [showIcon, setShowIcon] = useState(false);
+  const [mute, setMute] = useState(false);
+
+  const handleSound = () => {
+    setMute((prev)=>!prev);
+  }
 
   const handleToggle = () => {
     const video = videoRef.current;
@@ -30,17 +35,17 @@ export default function Reel({ reel }) {
         className="h-full w-full object-cover"
         autoPlay
         loop
-        muted
+        muted={mute}
         playsInline
       />
 
       <div 
       className={`absolute transition-opacity duration-300
       ${showIcon?"opacity-100": "opacity-0"}`}>
-        <div className='bg-black/40 p-4 rounded-full text-white'>
-          {paused? <Play size={60} />: <Pause size={60} />}
-        </div>
-        <div className='bg-black/40 p-4 rounded-full text-white'>
+        <div className='relative bg-black/40 p-4 rounded-full text-white'>
+          <div onClick={handleSound} className='absolute bg-black/40 -ml-2 -top-20 mb-4 p-4 rounded-full text-white'>
+            {mute? <VolumeX size={40} />: <Volume2 size={40} />}
+          </div>
           {paused? <Play size={60} />: <Pause size={60} />}
         </div>
       </div>
