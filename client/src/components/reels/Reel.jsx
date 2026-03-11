79e91e62b8df9b2efd47ccab22123f98465cf67b
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-11 12:20:55 +0530

    added pause play buttons and functionality

--- client/src/components/reels/Reel.jsx
+++ client/src/components/reels/Reel.jsx
@@ -1,16 +1,25 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
+import {Play, Pause, Volume2} from 'lucide-react';
 
 export default function Reel({ reel }) {
   const videoRef = useRef(null);
+  const [paused, setPaused] = useState(false);
+  const [showIcon, setShowIcon] = useState(false);
+
   const handleToggle = () => {
     const video = videoRef.current;
     if (!video) return;
 
     if (video.paused) {
       video.play();
+      setPaused(false);
     } else {
       video.pause();
+      setPaused(true);
     }
+
+    setShowIcon(true);
+    setTimeout(() => setShowIcon(false), 700);
   }
   return (
     <div onClick={handleToggle} className="h-screen w-full snap-start bg-black flex items-center justify-center">
@@ -24,6 +33,17 @@ export default function Reel({ reel }) {
         muted
         playsInline
       />
+
+      <div 
+      className={`absolute transition-opacity duration-300
+      ${showIcon?"opacity-100": "opacity-0"}`}>
+        <div className='bg-black/40 p-4 rounded-full text-white'>
+          {paused? <Play size={60} />: <Pause size={60} />}
+        </div>
+        <div className='bg-black/40 p-4 rounded-full text-white'>
+          {paused? <Play size={60} />: <Pause size={60} />}
+        </div>
+      </div>
 
     </div>
   );
