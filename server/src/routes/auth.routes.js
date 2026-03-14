c5bcfa2f6fac54853930705096d0ea3997a92906
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-14 14:40:59 +0530

    added multer middleware for register partner controller

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import { registerUser, registerPartner, loginUser, logoutUser } from '../controllers/auth.controller.js';
+import upload from '../middleware/multer.middeware.js';
 
 const router = Router();
 
 router.post("/register", registerUser, registerPartner);
-router.post("/register-partner", registerPartner)
+router.post("/register-partner", upload.single("heroImage"), registerPartner)
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
