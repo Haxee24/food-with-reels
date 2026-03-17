5563c00243efbbec915808f3c424850cbc852a57
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-17 16:19:42 +0530

    triggering mobile view for desktop browsers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(()=>{
+    if (window.innerWidth > 768) {
+      document.body.classList.add("force-mobile");
+    }
+  }, []);
+
   return (
     <AppRoutes />
   )
