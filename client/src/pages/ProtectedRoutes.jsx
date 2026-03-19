a38a06d9828fdd410136b3e15e87239b40a21594
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-19 18:00:02 +0530

    created Protected Routes component

--- client/src/pages/ProtectedRoutes.jsx
+++ client/src/pages/ProtectedRoutes.jsx
@@ -1,3 +1,10 @@
 import { Outlet, Navigate } from "react-router";
 import { useAuth } from "../context/auth/authContext";
 
+export default function ProtectedRoutes() {
+    const { user: isAuthenticated } = useAuth();
+    if (!isAuthenticated){
+        return <Navigate to={"/"} />
+    }
+    return <Outlet/>
+}
