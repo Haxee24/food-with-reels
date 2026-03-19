4f1e2b069d3f1638cce5454a4c4e30bf1cdc9959
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-03-19 17:39:02 +0530

    modified auth routes on the backend

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import {Router} from 'express';
-import { registerUser, registerPartner, loginUser, logoutUser } from '../controllers/auth.controller.js';
+import authenticateToken from '../middleware/auth.middleware.js';
+import { registerUser, registerPartner, loginUser, logoutUser, getUser } from '../controllers/auth.controller.js';
 import upload from '../middleware/multer.middeware.js';
 
 const router = Router();
@@ -8,5 +9,6 @@ router.post("/register", registerUser, registerPartner);
 router.post("/register-partner", upload.single("heroImage"), registerPartner)
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/getUser", authenticateToken, getUser);
 
 export default router;
