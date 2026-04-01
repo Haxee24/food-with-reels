529bb41855622ec62d5ea953ad07eed7a8730cb7
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-04-01 15:08:42 +0530

    configured backend routes to support search

--- server/src/routes/store.routes.js
+++ server/src/routes/store.routes.js
@@ -4,6 +4,7 @@ import { getFoodItems, getNewStores } from '../controllers/store.controller.js';
 const router = new Router();
 
 router.get("/", getNewStores);
+router.get("/all", getNewStores);
 router.get("/:id", getFoodItems);
 
 export default router;
