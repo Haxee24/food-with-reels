d73cdd41d2423e90e94a9661a2bc283d5026991a
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-04-05 02:49:30 +0530

    directory restructuring and changing imports

--- client/src/pages/general/customer/SearchPage.jsx
+++ client/src/pages/general/customer/SearchPage.jsx
@@ -28,7 +28,6 @@ export default function SearchPage() {
 
     return (
         <div className="min-h-screen bg-white dark:bg-black">
-
         {/* Page Header */}
         <div className="p-4 border-b border-gray-200 dark:border-neutral-800">
             <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
-import ReelsPage from './general/ReelsPage';
+import ReelsPage from './general/customer/ReelsPage';
 import Welcome from './general/Welcome';
 import Signin from './auth/Signin';
-import StoresPage from './general/StoresPage'
+import StoresPage from './general/customer/StoresPage'
 import Signup from './auth/Signup';
-import SearchPage from './general/SearchPage';
-import StoreShowcase from './general/StoreShowcase';
+import SearchPage from './general/customer/SearchPage';
+import StoreShowcase from './general/customer/StoreShowcase';
 export {ReelsPage, Signin, Signup, Welcome, StoresPage, SearchPage, StoreShowcase};
