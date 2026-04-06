1c41f100949a5e5108b16b9bdf5fab1f0b80ea81
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-04-06 17:52:12 +0530

    fixed minor export bug

--- server/src/controllers/customer.controller.js
+++ server/src/controllers/customer.controller.js
@@ -3,4 +3,7 @@ import { FoodReel } from "../models/index.js";
 const getNewReels = asyncHandler(async (req, res) => {
     const reels = await FoodReel.find().sort({ updatedAt: 1 }).limit(10);
     return res.status(200).json(new ApiResponse(200, reels, "Reels retrieval successful"));
-} )
+} );
+
+export {getNewReels};
+
